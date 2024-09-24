ceff503eb5d91509f434a11e45509063bf432723
kyanpepper <kyankks1@gmail.com>
2024-09-24 12:53:19 -0700

    Intial commit

---
+++ cachesim.h
@@ -0,0 +1,22 @@
+#include <getopt.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <math.h>
+#include <errno.h>
+
+#define ADDRESS_LENGTH 64  // 64-bit memory addressing
+
+
+/* 
+ * this function provides a standard way for your cache
+ * simulator to display its final statistics (i.e., hit and miss)
+ */ 
+extern void print_summary(int hits, int misses, int evictions);
+
+
+/* 
+ * this function provides a standard way for your cache
+ * simulator to display usage information
+ */
+extern void print_usage(char* argv[]);
